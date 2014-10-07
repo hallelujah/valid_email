13192cfdd93213232e50431a6f2d57c6aa9eb53c
Joel Chippindale <joel@joelchippindale.com>
2014-10-07 21:33:51 +0100

    Replace `be_true`/`be_false` with `be_truthy`/`be_falsey`
    Because `be_true` and `be_false` were deprecated in rspec v2.99, see
    https://github.com/rspec/rspec-expectations/blob/master/Changelog.md#2990beta1--2013-11-07

--- spec/extensions_validator_spec.rb
+++ spec/extensions_validator_spec.rb
@@ -6,17 +6,17 @@ describe String do
   it { "mymail@gmail".should respond_to(:email?) }
 
   it "is a valid e-mail" do
-    "mymail@gmail.com".email?.should be_true
+    "mymail@gmail.com".email?.should be_truthy
   end
 
   it "is not valid when text is not a real e-mail" do
-    "invalidMail".email?.should be_false
+    "invalidMail".email?.should be_falsey
   end
 
   context "when nil" do
 
     it "is invalid e-mail" do
-      nil.email?.should be_false
+      nil.email?.should be_falsey
     end
 
   end

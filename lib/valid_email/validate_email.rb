2f85e65536d81789bc329ae149d3aef313964640
Joel Chippindale <joel@joelchippindale.com>
2014-10-07 19:07:40 +0100

    Be specific about which exceptions to rescue
    The only exception we are expecting is a Mail::Field::ParseError and so this
    is the only exception we should rescue from.
    
    Rescuing from other exceptions may mask other problems for example in issue #33
    when all emails were being marked as invalid.

--- lib/valid_email/validate_email.rb
+++ lib/valid_email/validate_email.rb
@@ -9,19 +9,20 @@ class ValidateEmail
         m = Mail::Address.new(value)
         # We must check that value contains a domain and that value is an email address
         r = m.domain && m.address == value
+        if r
+          # Check that domain consists of dot-atom-text elements > 1
+          # In mail 2.6.1, domains are invalid per rfc2822 are parsed when they shouldn't
+          # This is to make sure we cover those cases
+          domain_dot_elements = m.domain.split(/\./)
+          r &&= domain_dot_elements.none?(&:blank?) && domain_dot_elements.size > 1
 
-        # Check that domain consists of dot-atom-text elements > 1
-        # In mail 2.6.1, domains are invalid per rfc2822 are parsed when they shouldn't
-        # This is to make sure we cover those cases
-        domain_dot_elements = m.domain.split(/\./)
-        r &&= domain_dot_elements.none?(&:blank?) && domain_dot_elements.size > 1
-
-        # Check if domain has DNS MX record
-        if r && options[:mx]
-          require 'valid_email/mx_validator'
-          r &&= mx_valid?(email)
+          # Check if domain has DNS MX record
+          if r && options[:mx]
+            require 'valid_email/mx_validator'
+            r &&= mx_valid?(email)
+          end
         end
-      rescue Exception => e
+      rescue Mail::Field::ParseError
         r = false
       end
       r
@@ -39,7 +40,7 @@ class ValidateEmail
           end
           r = mx.size > 0
         end
-      rescue Exception =>e
+      rescue Mail::Field::ParseError
         r = false
       end
       r
@@ -54,10 +55,10 @@ class ValidateEmail
       begin
         m = Mail::Address.new(value)
         r = !BanDisposableEmailValidator.config.include?(m.domain) if m.domain
-      rescue Exception => e
+      rescue Mail::Field::ParseError
         r = false
       end
-      
+
       r
     end
 

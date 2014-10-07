06e29ce73cc6eb0e6b6edafe924fde8d417b4978
Joel Chippindale <joel@joelchippindale.com>
2014-10-07 19:11:58 +0100

    Stop rescuing exceptions in `EmailValidator#validate_each`
    Because there don't seem to be any exceptions that we are expecting and would
    genuinely want to rescue from.

--- lib/valid_email/email_validator.rb
+++ lib/valid_email/email_validator.rb
@@ -4,26 +4,22 @@ require 'mail'
 require 'valid_email/validate_email'
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
-    begin
-      return if options[:allow_nil] && value.nil?
-      return if options[:allow_blank] && value.blank?
+    return if options[:allow_nil] && value.nil?
+    return if options[:allow_blank] && value.blank?
 
-      r = ValidateEmail.valid?(value)
-      # Check if domain has DNS MX record
-      if r && options[:mx]
-        require 'valid_email/mx_validator'
-        r &&= MxValidator.new(:attributes => attributes).validate(record)
-      elsif r && options[:mx_with_fallback]
-        require 'valid_email/mx_with_fallback_validator'
-        r &&= MxWithFallbackValidator.new(:attributes => attributes).validate(record)
-      end
-      # Check if domain is disposable
-      if r && options[:ban_disposable_email]
-        require 'valid_email/ban_disposable_email_validator'
-        r &&= BanDisposableEmailValidator.new(:attributes => attributes).validate(record)
-      end
-    rescue Exception => e
-      r = false
+    r = ValidateEmail.valid?(value)
+    # Check if domain has DNS MX record
+    if r && options[:mx]
+      require 'valid_email/mx_validator'
+      r &&= MxValidator.new(:attributes => attributes).validate(record)
+    elsif r && options[:mx_with_fallback]
+      require 'valid_email/mx_with_fallback_validator'
+      r &&= MxWithFallbackValidator.new(:attributes => attributes).validate(record)
+    end
+    # Check if domain is disposable
+    if r && options[:ban_disposable_email]
+      require 'valid_email/ban_disposable_email_validator'
+      r &&= BanDisposableEmailValidator.new(:attributes => attributes).validate(record)
     end
     record.errors.add attribute, (options[:message] || I18n.t(:invalid, :scope => "valid_email.validations.email")) unless r
   end

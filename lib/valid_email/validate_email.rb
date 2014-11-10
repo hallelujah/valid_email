5b20de3b5b9ac9efde0ddd5847433b12b88837d2
Yuriy Naidyon <yurokle@gmail.com>
2014-11-10 15:34:02 +0200

    Refactoring: code cleanup

--- lib/valid_email/validate_email.rb
+++ lib/valid_email/validate_email.rb
@@ -1,51 +1,47 @@
 class ValidateEmail
-
   class << self
-
     def valid?(value, user_options={})
-      options = { :mx => false, :message => nil }.merge!(user_options)
-      r       = false
-      begin
-        m = Mail::Address.new(value)
-        # We must check that value contains a domain and that value is an email address
-        r = m.domain && m.address == value
-        if r
-          # Check that domain consists of dot-atom-text elements > 1 and does not
-          # contain spaces.
-          #
-          # In mail 2.6.1, domains are invalid per rfc2822 are parsed when they shouldn't
-          # This is to make sure we cover those cases
-          domain_dot_elements = m.domain.split(/\./)
-          r = m.domain.match(/^\S+$/) && domain_dot_elements.none?(&:blank?) && domain_dot_elements.size > 1
+      options = { :mx => false, :message => nil }.merge(user_options)
 
-          # Check if domain has DNS MX record
-          if r && options[:mx]
-            require 'valid_email/mx_validator'
-            r = mx_valid?(value)
-          end
-        end
-      rescue Mail::Field::ParseError
-        r = false
+      m = Mail::Address.new(value)
+      # We must check that value contains a domain and that value is an email address
+      return false unless m.domain && m.address == value
+
+      # Check that domain consists of dot-atom-text elements > 1 and does not
+      # contain spaces.
+      #
+      # In mail 2.6.1, domains are invalid per rfc2822 are parsed when they shouldn't
+      # This is to make sure we cover those cases
+
+      return false unless m.domain.match(/^\S+$/)
+
+      domain_dot_elements = m.domain.split(/\./)
+      return false unless domain_dot_elements.size > 1 && domain_dot_elements.all?(&:present?)
+
+      # Check if domain has DNS MX record
+      if options[:mx]
+        require 'valid_email/mx_validator'
+        return mx_valid?(value)
       end
-      r
+
+      true
+    rescue Mail::Field::ParseError
+      false
     end
 
     def mx_valid?(value, fallback=false)
-      r = false
-      begin
-        m = Mail::Address.new(value)
-        if m.domain
-          mx = []
-          Resolv::DNS.open do |dns|
-            mx.concat dns.getresources(m.domain, Resolv::DNS::Resource::IN::MX)
-            mx.concat dns.getresources(m.domain, Resolv::DNS::Resource::IN::A) if fallback
-          end
-          r = mx.size > 0
-        end
-      rescue Mail::Field::ParseError
-        r = false
+      m = Mail::Address.new(value)
+      return false unless m.domain
+
+      mx = []
+      Resolv::DNS.open do |dns|
+        mx.concat dns.getresources(m.domain, Resolv::DNS::Resource::IN::MX)
+        mx.concat dns.getresources(m.domain, Resolv::DNS::Resource::IN::A) if fallback
       end
-      r
+
+      return mx.any?
+    rescue Mail::Field::ParseError
+      false
     end
 
     def mx_valid_with_fallback?(value)
@@ -53,17 +49,10 @@ class ValidateEmail
     end
 
     def ban_disposable_email?(value)
-      r = false
-      begin
-        m = Mail::Address.new(value)
-        r = !BanDisposableEmailValidator.config.include?(m.domain) if m.domain
-      rescue Mail::Field::ParseError
-        r = false
-      end
-
-      r
+      m = Mail::Address.new(value)
+      m.domain && !BanDisposableEmailValidator.config.include?(m.domain)
+    rescue Mail::Field::ParseError
+      false
     end
-
   end
-
 end

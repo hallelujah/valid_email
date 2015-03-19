a3df17e7b4ef712cb51f4e50538fb7016aa8f0fd
Evan Brynne <evan@hackerone.com>
2015-03-18 18:47:40 -0700

    Added local email segment validation

--- lib/valid_email/validate_email.rb
+++ lib/valid_email/validate_email.rb
@@ -1,5 +1,9 @@
 class ValidateEmail
   class << self
+    SPECIAL_CHARS = ['(', ')', ',', ':', ';', '<', '>', '@', '[', ']']
+    SPECIAL_ESCAPED_CHARS = [' ', '\\', '"']
+    LOCAL_MAX_LEN = 64
+
     def valid?(value, user_options={})
       options = { :mx => false, :message => nil }.merge(user_options)
 
@@ -18,8 +22,8 @@ class ValidateEmail
       domain_dot_elements = m.domain.split(/\./)
       return false unless domain_dot_elements.size > 1 && domain_dot_elements.all?(&:present?)
 
-      # Two dots next to each other or a dot at the beginning or end of the local part are not allowed by RFC2822.
-      return false unless m.local.split('.', -1).all?(&:present?)
+      # Ensure that the local segment adheres to adheres to RFC-5322
+      return false unless valid_local?(m.local)
 
       # Check if domain has DNS MX record
       if options[:mx]
@@ -30,6 +34,38 @@ class ValidateEmail
       true
     rescue Mail::Field::ParseError
       false
+    end
+
+    def valid_local?(local)
+      return false unless local.length <= LOCAL_MAX_LEN
+      # Emails can be validated by segments delineated by '.', referred to as dot atoms.
+      # See http://tools.ietf.org/html/rfc5322#section-3.2.3
+      return local.split('.', -1).all? { |da| valid_dot_atom?(da) }
+    end
+
+    def valid_dot_atom?(dot_atom)
+      # Leading, trailing, and double '.'s aren't allowed
+      return false if dot_atom.empty?
+      # A dot atom can be quoted, which allows use of the SPECIAL_CHARS
+      if dot_atom[0] == '"' || dot_atom[-1] == '"'
+        # A quoted segment must have leading and trailing '"#"'s
+        return false if dot_atom[0] != dot_atom[-1]
+
+        # Excluding the bounding quotes, all of the SPECIAL_ESCAPED_CHARS must have a leading '\'
+        index = dot_atom.length - 2
+        while index > 0
+          if SPECIAL_ESCAPED_CHARS.include? dot_atom[index]
+            return false if index == 1 || dot_atom[index - 1] != '\\'
+            # On an escaped special character, skip an index to ignore the '\' that's doing the escaping
+            index -= 1
+          end
+          index -= 1
+        end
+      else
+        # If we're not in a quoted dot atom then no special characters are allowed.
+        return false unless ((SPECIAL_CHARS | SPECIAL_ESCAPED_CHARS) & dot_atom.split('')).empty?
+      end
+      return true 
     end
 
     def mx_valid?(value, fallback=false)

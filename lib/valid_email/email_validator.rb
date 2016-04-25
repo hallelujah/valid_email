8c49969a33615ca7648c47b0128ac323d4d4da1e
Jochen Niebuhr <jniebuhr@cfire.de>
2016-04-25 15:23:23 +0200

    Fix domain validation behavior

--- lib/valid_email/email_validator.rb
+++ lib/valid_email/email_validator.rb
@@ -14,6 +14,9 @@ class EmailValidator < ActiveModel::EachValidator
     elsif r && options[:mx_with_fallback]
       require 'valid_email/mx_with_fallback_validator'
       r = MxWithFallbackValidator.new(:attributes => attributes, message: options[:message]).validate(record)
+    elsif r && options[:domain]
+      require 'valid_email/domain_validator'
+      r = DomainValidator.new(:attributes => attributes, message: options[:message]).validate(record)
     end
     # Check if domain is disposable
     if r && options[:ban_disposable_email]

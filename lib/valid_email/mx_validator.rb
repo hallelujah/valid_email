3f5204fb00b6c494db5afb9bbcadc63ac5c93ede
Anthony Laibe <anthony@laibe.cc>
2017-03-09 11:24:14 +0000

    Allow to configure timeouts for dns

--- lib/valid_email/mx_validator.rb
+++ lib/valid_email/mx_validator.rb
@@ -3,6 +3,12 @@ require 'active_model/validations'
 require 'resolv'
 require 'valid_email/validate_email'
 class MxValidator < ActiveModel::EachValidator
+  def self.config
+    @@config = { timeouts: [] } unless defined? @@config
+
+    @@config
+  end
+
   def validate_each(record,attribute,value)
     r = ValidateEmail.mx_valid?(value)
     record.errors.add attribute, (options[:message] || I18n.t(:invalid, :scope => "valid_email.validations.email")) unless r

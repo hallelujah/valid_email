cfa5c28e266720b957c394a7796f4ee3c8ac7cd9
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2021-02-24 19:51:23 +0100

    Update RSpec to version 3.10
    I extended also the RSpec configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,8 @@ $:.unshift File.expand_path('../../lib',__FILE__)
 require 'valid_email'
 
 RSpec.configure do |config|
+  config.order = :random
+  config.profile_examples = 10
   config.raise_errors_for_deprecations!
+  Kernel.srand config.seed
 end
